737770d5dbb6e99efc5b42187975b261e1f674aa
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-01 18:45:43 +0100

    Add tests and refactor solution

--- day01/Program.cs
+++ day01/Program.cs
@@ -3,52 +3,50 @@
 var lines = File.ReadAllLines("input.txt")
     .ToArray<string>();
 
-PartOne(lines, "[1-9]");
-PartOne(lines, "[1-9]|one|two|three|four|five|six|seven|eight|nine");
+var part1Result = SumOfCalibrationValues(lines, "[1-9]");
+System.Console.WriteLine($"Part 1: Sum of calibration values: {part1Result}");
 
+var part2Result = SumOfCalibrationValues(lines, "[1-9]|one|two|three|four|five|six|seven|eight|nine");
+System.Console.WriteLine($"Part 2: Sum of calibration values: {part2Result}");
 
-static void PartOne(string[] lines, string matchString)
+static int SumOfCalibrationValues(string[] lines, string matchString)
 {
     int total = 0;
     foreach (var line in lines)
     {
         var ct = new CalibrationThing(line, matchString);
-        var firstDigit = ct.FirstDigit;
-        var lastDigit = ct.LastDigit;
-
-        var number = Convert.ToInt32($"{firstDigit}{lastDigit}");
-
-        total += number;
+        total += ct.CalibrationValue;
     }
 
-    System.Console.WriteLine(total);
+    return total;
 }
 
 public record CalibrationThing(string Line, string MatchString)
 {
-    private readonly Regex regex1 = new(MatchString);
-    private readonly Regex regex2 = new(MatchString, RegexOptions.RightToLeft);
+    private readonly Regex regexLeftToRight = new(MatchString);
+    private readonly Regex regexRightToLeft = new(MatchString, RegexOptions.RightToLeft);
+    private readonly Dictionary<string, string> textLookup = new()
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" },
+    };
+
     public char FirstDigit
     {
         get
         {
-            var matches = regex1.Matches(Line);
+            var matches = regexLeftToRight.Matches(Line);
             var match = matches.First().Value;
             if (match.Length > 1)
             {
-                match = match switch
-                {
-                    "one" => "1",
-                    "two" => "2",
-                    "three" => "3",
-                    "four" => "4",
-                    "five" => "5",
-                    "six" => "6",
-                    "seven" => "7",
-                    "eight" => "8",
-                    "nine" => "9",
-                    _ => "X"
-                };
+                match = textLookup[match];
             }
             return Convert.ToChar(match);
         }
@@ -57,26 +55,16 @@ public record CalibrationThing(string Line, string MatchString)
     {
         get
         {
-            var matches = regex2.Matches(Line);
+            var matches = regexRightToLeft.Matches(Line);
             var match = matches.First().Value;
             if (match.Length > 1)
             {
-                match = match switch
-                {
-                    "one" => "1",
-                    "two" => "2",
-                    "three" => "3",
-                    "four" => "4",
-                    "five" => "5",
-                    "six" => "6",
-                    "seven" => "7",
-                    "eight" => "8",
-                    "nine" => "9",
-                    _ => "X"
-                };
+                match = textLookup[match];
             }
             return Convert.ToChar(match);
         }
     }
+
+    public int CalibrationValue => Convert.ToInt32($"{FirstDigit}{LastDigit}");
 }
 
