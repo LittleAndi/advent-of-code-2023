6c6fe7a0009059e1c37c99158c9b5b6ff1f623f7
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-02 11:24:33 +0100

    Solution for part 2

--- tests/day02tests/UnitTest1.cs
+++ tests/day02tests/UnitTest1.cs
@@ -19,4 +19,20 @@ public class UnitTest1
         game.Blues.ShouldBe(blues);
         game.IsValidGame.ShouldBe(validGame);
     }
+
+    [Theory]
+    [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 4, 2, 6, 48)]
+    [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 1, 3, 4, 12)]
+    [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 20, 13, 6, 1560)]
+    [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 14, 3, 15, 630)]
+    [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 6, 3, 2, 36)]
+
+    public void ShouldReturnMiniumCountOfColors(string input, int minReds, int minGreens, int minBlues, int power)
+    {
+        var game = new Game(input, 0, 0, 0);
+        game.MiniumReds.ShouldBe(minReds);
+        game.MiniumGreen.ShouldBe(minGreens);
+        game.MiniumBlue.ShouldBe(minBlues);
+        game.Power.ShouldBe(power);
+    }
 }
