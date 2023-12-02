65b36a982ddbd0ece0e4ea1c311a3c4e0653afbe
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-02 11:10:22 +0100

    Solution for part 1

--- day02/Program.cs
+++ day02/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+var lines = File.ReadAllLines("input.txt")
+    .ToArray<string>();
+
+var sumOfGameIds = GetSumOfGameIds(lines);
+System.Console.WriteLine($"Sum of valid game ids: {sumOfGameIds}");
+
+int GetSumOfGameIds(string[] lines)
+{
+    var sumOfGameIds = 0;
+    foreach (var line in lines)
+    {
+        var game = new Game(line, 12, 13, 14);
+        if (game.IsValidGame)
+        {
+            System.Console.WriteLine($"Game {game.Id} is a valid game; {game.Reds} {game.Greens} {game.Blues}");
+            sumOfGameIds += game.Id;
+        }
+        else
+        {
+            System.Console.WriteLine($"Game {game.Id} is not valid game; {game.Reds} {game.Greens} {game.Blues}");
+        }
+
+    }
+    return sumOfGameIds;
+}
+
+public class Game
+{
+    private readonly int gameId;
+    private readonly List<Dictionary<string, int>> sets = [];
+    private readonly string input;
+    private readonly int bagReds;
+    private readonly int bagGreens;
+    private readonly int bagBlues;
+
+    public Game(string input, int bagReds, int bagGreens, int bagBlues)
+    {
+        var gameInfo = input.Split(':')[0].Remove(0, 5);
+        gameId = Convert.ToInt32(gameInfo);
+
+        var setsInfo = input.Split(':')[1].Trim()
+                        .Split(';')
+                        .Select(set => set.Trim());
+
+        foreach (var setInfo in setsInfo)
+        {
+            var set = new Dictionary<string, int>();
+
+            var cubes = setInfo.Split(',').Select(cube => cube.Trim());
+
+            foreach (var cube in cubes)
+            {
+                var count = Convert.ToInt32(cube.Split(' ')[0]);
+                var color = cube.Split(' ')[1];
+                if (!set.TryAdd(color, count)) set[color] += count;
+            }
+
+            sets.Add(set);
+        }
+
+        this.input = input;
+        this.bagReds = bagReds;
+        this.bagGreens = bagGreens;
+        this.bagBlues = bagBlues;
+    }
+
+    public int Id => gameId;
+
+    public int Reds => sets.Where(s => s.ContainsKey("red")).Sum(s => s["red"]);
+    public int Greens => sets.Where(s => s.ContainsKey("green")).Sum(s => s["green"]);
+    public int Blues => sets.Where(s => s.ContainsKey("blue")).Sum(s => s["blue"]);
+    public bool IsValidGame
+    {
+        get
+        {
+            foreach (var set in sets)
+            {
+                if (set.TryGetValue("red", out int reds) && reds > bagReds) return false;
+                if (set.TryGetValue("green", out int greens) && greens > bagGreens) return false;
+                if (set.TryGetValue("blue", out int blues) && blues > bagBlues) return false;
+            }
+            return true;
+        }
+    }
+}
--- tests/day02tests/UnitTest1.cs
+++ tests/day02tests/UnitTest1.cs
@@ -2,9 +2,21 @@ namespace day02tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
-    {
+    [Theory]
+    [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 1, 4 + 1, 2 + 2, 3 + 6, true)]
+    [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 2, 1, 2 + 3 + 1, 1 + 4 + 1, true)]
+    [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 3, 20 + 4 + 1, 8 + 13 + 5, 6 + 5, false)]
+    [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 4, 3 + 6 + 14, 1 + 3 + 3, 6 + 15, false)]
+    [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 5, 6 + 1, 3 + 2, 1 + 2, true)]
+    [InlineData("Game 99: 2 red, 2 green; 1 red, 1 green, 2 blue; 3 blue, 3 red, 3 green; 1 blue, 3 green, 7 red; 5 red, 3 green, 1 blue", 99, 2 + 1 + 3 + 7 + 5, 2 + 1 + 3 + 3 + 3, 2 + 3 + 1 + 1, true)]
 
+    public void Test1(string input, int gameId, int reds, int greens, int blues, bool validGame)
+    {
+        var game = new Game(input, 12, 13, 14);
+        game.Id.ShouldBe(gameId);
+        game.Reds.ShouldBe(reds);
+        game.Greens.ShouldBe(greens);
+        game.Blues.ShouldBe(blues);
+        game.IsValidGame.ShouldBe(validGame);
     }
 }
