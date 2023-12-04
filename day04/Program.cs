0f1d952ad4d5b3d6dc23d30a027a63f212693c43
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-04 07:38:51 +0100

    Quick one for the first part of day 4

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt")
+    .ToArray<string>();
+
+System.Console.WriteLine(lines.Select(l => new Card(l)).Sum(card => card.Points));
+
+
+public class Card
+{
+    private readonly HashSet<int> winningNumbers = [];
+    private readonly HashSet<int> numbersYouHave = [];
+
+    public Card(string input)
+    {
+        winningNumbers = input.Split(':')[1].Split('|')[0].Split(' ').Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => Convert.ToInt32(n)).ToHashSet();
+        numbersYouHave = input.Split(':')[1].Split('|')[1].Split(' ').Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => Convert.ToInt32(n)).ToHashSet();
+    }
+
+    public int Points
+    {
+        get
+        {
+            return (int)Math.Pow(2, winningNumbers.Intersect(numbersYouHave).Count() - 1);
+        }
+    }
+}
