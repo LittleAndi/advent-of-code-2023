072eb603aa38601b55aef69088c7fdf9bf069ed6
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-05 08:08:08 +0100

    Solution for part 1 of day 5

--- day05/Program.cs
+++ day05/Program.cs
@@ -1,2 +1,112 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Contracts;
+
+var lines = File.ReadAllLines("input.txt")
+    .ToArray<string>();
+
+var almanac = new Almanac(lines);
+System.Console.WriteLine(almanac.SeedLocations.Min());
+
+public class Almanac
+{
+    public enum PlantFactorMapping
+    {
+        FertilizerToWaterMap,
+        HumidityToLocationMap,
+        LightToTemperatureMap,
+        SeedToSoilMap,
+        SoilToFertilizerMap,
+        TemperatureToHumidityMap,
+        WaterToLightMap
+    }
+
+    private readonly long[] seeds = [];
+    private readonly List<KeyValuePair<string, MappingLine>> mapping = [];
+
+    public Almanac(string[] input)
+    {
+        var pos = 0;
+
+        // seeds
+        seeds = input[pos++].Remove(0, 7).Split(' ').Select(seed => Convert.ToInt64(seed)).ToArray();
+
+        var currentMap = "";
+        while (pos < input.Length)
+        {
+            if (string.IsNullOrWhiteSpace(input[pos]))
+            {
+                pos++;
+                continue;
+            }
+
+            if (char.IsLetter(input[pos][0]))
+            {
+                currentMap = input[pos];
+                pos++;
+                continue;
+            }
+
+            long sourceCategoryStart = Convert.ToInt64(input[pos].Split(' ')[0]);
+            long destinationCategoryStart = Convert.ToInt64(input[pos].Split(' ')[1]);
+            long length = Convert.ToInt64(input[pos].Split(' ')[2]);
+
+            mapping.Add(new KeyValuePair<string, MappingLine>(currentMap, new MappingLine(sourceCategoryStart, destinationCategoryStart, length)));
+
+            pos++;
+        }
+
+
+    }
+
+    // Used for testing the input mapping
+    public long CategoryMapLineCount(string categoryMap) => mapping.Where(m => m.Key.Equals(categoryMap)).Count();
+
+    public long[] SeedLocations
+    {
+        get
+        {
+            List<long> seedLocations = [];
+            foreach (var seed in seeds)
+            {
+                long currentPosition = seed;
+
+                // seed-to-soil map:
+                currentPosition = GetDestination("seed-to-soil map:", currentPosition);
+
+                // soil-to-fertilizer map:
+                currentPosition = GetDestination("soil-to-fertilizer map:", currentPosition);
+
+                // fertilizer-to-water map:
+                currentPosition = GetDestination("fertilizer-to-water map:", currentPosition);
+
+                // water-to-light map:
+                currentPosition = GetDestination("water-to-light map:", currentPosition);
+
+                // light-to-temperature map:
+                currentPosition = GetDestination("light-to-temperature map:", currentPosition);
+
+                // temperature-to-humidity map:
+                currentPosition = GetDestination("temperature-to-humidity map:", currentPosition);
+
+                // humidity-to-location map:
+                currentPosition = GetDestination("humidity-to-location map:", currentPosition);
+
+                seedLocations.Add(currentPosition);
+            }
+            return [.. seedLocations];
+        }
+    }
+
+    private long GetDestination(string mapName, long source)
+    {
+        long destination = source;
+        var map = mapping.Where(m => m.Key.Equals(mapName)).FirstOrDefault(m => m.Value.IsMappedWithThisLine(source)).Value;
+        if (map != null) destination = map.Destination(source);
+        return destination;
+    }
+}
+
+public record MappingLine(long DestinationCategoryStart, long SourceCategoryStart, long Length)
+{
+    public bool IsMappedWithThisLine(long source) => source >= SourceCategoryStart && source < SourceCategoryStart + Length;
+    public long Destination(long source) => IsMappedWithThisLine(source) ? DestinationCategoryStart + (source - SourceCategoryStart) : -1;
+};
--- tests/day05tests/UnitTest1.cs
+++ tests/day05tests/UnitTest1.cs
@@ -1,10 +1,73 @@
 namespace day05tests;
 
+[Trait("Day", "5")]
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ShouldCreateAlmanacAndMapSeedLocations()
     {
+        var lines = new string[]
+        {
+            "seeds: 79 14 55 13",
+            "",
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48",
+            "",
+            "soil-to-fertilizer map:",
+            "0 15 37",
+            "37 52 2",
+            "39 0 15",
+            "",
+            "fertilizer-to-water map:",
+            "49 53 8",
+            "0 11 42",
+            "42 0 7",
+            "57 7 4",
+            "",
+            "water-to-light map:",
+            "88 18 7",
+            "18 25 70",
+            "",
+            "light-to-temperature map:",
+            "45 77 23",
+            "81 45 19",
+            "68 64 13",
+            "",
+            "temperature-to-humidity map:",
+            "0 69 1",
+            "1 0 69",
+            "",
+            "humidity-to-location map:",
+            "60 56 37",
+            "56 93 4",
+        };
 
+        var almanac = new Almanac(lines);
+        almanac.ShouldNotBeNull();
+        almanac.CategoryMapLineCount("seed-to-soil map:").ShouldBe(2);
+        almanac.CategoryMapLineCount("soil-to-fertilizer map:").ShouldBe(3);
+        almanac.CategoryMapLineCount("fertilizer-to-water map:").ShouldBe(4);
+        almanac.CategoryMapLineCount("water-to-light map:").ShouldBe(2);
+        almanac.CategoryMapLineCount("light-to-temperature map:").ShouldBe(3);
+        almanac.CategoryMapLineCount("temperature-to-humidity map:").ShouldBe(2);
+        almanac.CategoryMapLineCount("humidity-to-location map:").ShouldBe(2);
+        almanac.SeedLocations.ShouldBe([82, 43, 86, 35]);
+    }
+
+    [Fact]
+    public void ShouldMap()
+    {
+        var mappingLine = new MappingLine(52, 50, 48);
+        mappingLine.IsMappedWithThisLine(49).ShouldBeFalse();
+        mappingLine.IsMappedWithThisLine(50).ShouldBeTrue();
+        mappingLine.IsMappedWithThisLine(51).ShouldBeTrue();
+        mappingLine.IsMappedWithThisLine(52).ShouldBeTrue();
+
+        mappingLine.Destination(49).ShouldBe(-1);
+        mappingLine.Destination(50).ShouldBe(52);
+        mappingLine.Destination(51).ShouldBe(53);
+        mappingLine.Destination(52).ShouldBe(54);
+        mappingLine.Destination(79).ShouldBe(81);
     }
 }
