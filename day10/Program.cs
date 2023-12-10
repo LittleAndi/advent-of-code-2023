d76e1bc0bb94f979eea84f1d3f641e2ed3edf216
Lars Nyström <nystrom.lasse@gmail.com>
2023-12-10 18:42:50 +0100

    WIP day 10

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
 Console.WriteLine("Hello, World!");
+
+
+public class PipeMaze
+{
+    char[][] map;
+    List<(int i, int j, int step)> path = [];
+    private (int i, int j) positionS;
+
+    public PipeMaze(string[] input)
+    {
+        map = input.Select(line => line.ToCharArray()).ToArray();
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            if (map[i].Contains('S')) positionS = (i, Array.FindIndex(map[i], x => x == 'S'));
+        }
+
+        MapTheSteps();
+    }
+
+    private void MapTheSteps()
+    {
+        // Start at S
+        var pos = map[positionS.i][positionS.j];
+        var steps = 0;
+        bool weAreBackAtStart = false;
+
+        while (!weAreBackAtStart)
+        {
+            (int i, int j) = MoveNext(pos);
+        }
+    }
+
+    private (int i, int j) MoveNext(int iCurrent, int jCurrent)
+    {
+        return map[iCurrent][jCurrent] switch
+        {
+            '|'
+            _ => (1, 2)
+        };
+    }
+
+    public (int x, int y) PositionS => positionS;
+}
+
--- tests/day10tests/UnitTest1.cs
+++ tests/day10tests/UnitTest1.cs
@@ -1,10 +1,23 @@
+using Shouldly;
+using Xunit.Sdk;
+
 namespace day10tests;
 
+[Trait("Day", "10")]
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        string[] lines = [
+            "..F7.",
+            ".FJ|.",
+            "SJ.L7",
+            "|F--J",
+            "LJ...",
+        ];
+        var maze = new PipeMaze(lines);
+        maze.PositionS.ShouldBe((2, 0));
+        maze.TotalLapSteps.ShouldBe(16);
     }
 }
